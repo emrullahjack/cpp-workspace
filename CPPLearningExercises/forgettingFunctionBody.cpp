424b3ba1075cf4690d1d0046b0e5e6f2b4191b71
EmrullahJack <emrullahjack@gmail.com>
2020-12-12 06:39:35 -0500

    some basic cpp exercises added, for keeping skills alive

---
+++ CPPLearningExercises/forgettingFunctionBody.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int add(int x, int y);
+
+int main(int argc, char const *argv[]) {
+  std::cout << "The sum of 3 and for is " << add(3, 4) << '\n';
+  return 0;
+}
---
+++ LearningCPP/uninitializedVariables.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+  int x; // x has garbage value 
+
+  std::cout << x;
+
+  return 0;
+}
